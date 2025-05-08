2aa55ffb99be20eb1be65f5aa74b94ac7773567f
DSMejantel <149260856+DSMejantel@users.noreply.github.com>
2025-05-08 22:42:13 +0200

    icon + change etat

--- avancement/ferme.sql
+++ avancement/ferme.sql
@@ -3,7 +3,8 @@ SELECT 'redirect' AS component,
  WHERE NOT EXISTS (SELECT 1 FROM login_session WHERE id=sqlpage.cookie('session'));
 SET group_id = (SELECT user_info.groupe FROM login_session join user_info on user_info.username=login_session.username WHERE id = sqlpage.cookie('session'));
 
- UPDATE actions SET etat=1 WHERE id=$id
+ UPDATE actions SET etat=1 WHERE id=$id;
+ UPDATE actions SET avancement=100 WHERE id=$id
  RETURNING 
  'redirect' as component,
  '../risque_fiche?id='||$risque||'#'||$id as link;
--- avancement/view_ferme.sql
+++ avancement/view_ferme.sql
@@ -3,7 +3,8 @@ SELECT 'redirect' AS component,
  WHERE NOT EXISTS (SELECT 1 FROM login_session WHERE id=sqlpage.cookie('session'));
 SET group_id = (SELECT user_info.groupe FROM login_session join user_info on user_info.username=login_session.username WHERE id = sqlpage.cookie('session'));
 
- UPDATE actions SET etat=1 WHERE id=$fiche
+ UPDATE actions SET etat=1 WHERE id=$fiche;
+ UPDATE actions SET avancement=100 WHERE id=$fiche
  RETURNING 
  'redirect' as component,
  '../action_view.sql?id='||$id||'&fiche='||$fiche as link;
