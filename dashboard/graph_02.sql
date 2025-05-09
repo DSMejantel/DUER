cadaf8486e2ae9b3b4f2f863d9a03a607eaa5034
DSMejantel <149260856+DSMejantel@users.noreply.github.com>
2025-05-09 22:40:56 +0200

    corrections et améliorations

--- dashboard/graph_02.sql
+++ dashboard/graph_02.sql
@@ -3,10 +3,10 @@ select
     'Fiches actions' as title,
     'pie'     as type,
     350 as height,
-    'green' as color,
     'orange' as color,
+    'green' as color,
     TRUE      as labels;
 select 
     CASE WHEN etat=1 THEN 'Terminés' WHEN etat=0 THEN 'en cours' END as label,
-    count(distinct etat)    as value
+    count(etat)    as value
     FROM actions GROUP BY etat;
